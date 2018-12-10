06ac9f16e9c18310261b9abfbba34edb638d8bc8
Debbie Murphy <dmurphinohio@gmail.com>
2018-12-10 12:18:55 -0500

    created models

---
+++ FinalCapstone/Models/Restaurant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalCapstone.Models
+{
+    public class Restaurant
+    {
+        public int RestaurantId { get; set; }
+        public string RestaurantName { get; set; }
+        public TimeSpan OpenTime { get; set; }
+        public TimeSpan CloseTime { get; set; }
+        public TimeSpan OpenTimeWeekend { get; set; }
+        public TimeSpan CloseTimeWeekend { get; set; }
+    }
+}
---
+++ FinalCapstone/Models/UserFavorites.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalCapstone.Models
+{
+    public class UserFavorites
+    {
+        public int UserId { get; set; }
+        public int RestaurantId { get; set; }
+        public int FoodItemId { get; set; }
+    }
+}
