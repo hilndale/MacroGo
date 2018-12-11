95dc39f9eea2d396484bc9c2aae54d0dcce0a063
Debbie Murphy <dmurphinohio@gmail.com>
2018-12-11 10:15:13 -0500

    added update food item DAL

--- FinalCapstone/Dal/FoodItemSqlDAL.cs
+++ FinalCapstone/Dal/FoodItemSqlDAL.cs
@@ -71,16 +71,46 @@ namespace FinalCapstone.Dal
                     }
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
 
-            } 
+            }
             return result;
         }
 
         public bool UpdateFoodItem(FoodList food)
         {
-            return false;
+            bool result = false;
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand(@"UPDATE Food SET [Food_Item]=<@Food_Item>,[Restaurant_Id]=<@Restaurant_Id>, [Calories]=<@Calories>, [Total_Fat_g}=<@Total_Fat_g>, [Carbohydrates_g]=<Carbohydrates_g>, [Protein_g]=<Protein_g> WHERE Food_Id = @Food_Id;", conn);
+
+                    cmd.Parameters.AddWithValue("@Food_Id", food.FoodId);
+                    cmd.Parameters.AddWithValue("@Restaurant_Id", food.RestaurantId);
+                    cmd.Parameters.AddWithValue("@Calories", food.Calories);
+                    cmd.Parameters.AddWithValue("@Total_Fat_g", food.Fat);
+                    cmd.Parameters.AddWithValue("@Carbohydrates_g", food.Carbs);
+                    cmd.Parameters.AddWithValue("@Protein_g", food.Protein);
+
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    if (rowsAffected == 1)
+                    {
+                        result = true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return result;
+
         }
 
     }
