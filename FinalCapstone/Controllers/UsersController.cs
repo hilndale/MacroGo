7ec00ad829d191999642f890df75650a819a289d
Keelie Bente <keelie.bente@gmail.com>
2018-12-12 13:01:24 -0500

    Trying to figure out sessions

--- FinalCapstone/Controllers/UsersController.cs
+++ FinalCapstone/Controllers/UsersController.cs
@@ -3,11 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using FinalCapstone.Dal;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.Web;
 
 namespace FinalCapstone.Controllers
 {
     public class UsersController : Controller
     {
+        private readonly IUserDAL _userDAL;
+
+        public UsersController(IUserDAL userDAL)
+        {
+            _userDAL = userDAL;
+        }
+
+        [ChildActionOnly]
+        public ActionResult Navigation()
+        {
+            if (Session[SessionKeys.Username] == null)
+            {
+                return PartialView("_AnonymousNav");
+            }
+            else
+            {
+                return PartialView("_AuthenticatedNav");
+            }
+        }
+
         public IActionResult Index()
         {
             return View();
