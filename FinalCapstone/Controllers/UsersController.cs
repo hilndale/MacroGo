4780a74d2cdf0a38eed6a7ea46cfd990710c11c5
Keelie Bente <keelie.bente@gmail.com>
2018-12-12 14:49:43 -0500

    Fleshed out usercontroller logic, added viewmodels for login and register

--- FinalCapstone/Controllers/UsersController.cs
+++ FinalCapstone/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 ﻿using FinalCapstone.Dal;
 using FinalCapstone.Extensions;
-using System.Web;
+using FinalCapstone.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FinalCapstone.Controllers
@@ -12,6 +13,109 @@ namespace FinalCapstone.Controllers
         public UsersController(IUserDAL userDAL)
         {
             _userDAL = userDAL;
+        }
+
+        public IActionResult Navigation()
+        {
+            if (HttpContext.Session.Get(SessionKeys.Username) == null)
+            {
+                return PartialView("_AnonymousNav");
+            }
+            else
+            {
+                return PartialView("_AuthenticatedNav");
+            }
+        }
+
+        // GET: User/Index
+        public ActionResult Index()
+        {
+            if (HttpContext.Session.Get(SessionKeys.Username) == null)
+            {
+                return RedirectToAction("Login");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        public ActionResult Login()
+        {
+            return View("Login");
+        }
+
+        // POST: User/Login
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Login", model);
+            }
+
+            Users user = _userDAL.GetUser(model.Email);
+            // user does not exist or password is wrong
+            if (user == null || user.Password != model.Password)
+            {
+                ModelState.AddModelError("invalid-credentials", "An invalid username or password was provided");
+                return View("Login", model);
+            }
+            else
+            {
+                //FormsAuthentication.SetAuthCookie(user.Email, true);
+                HttpContext.Session.Set(user.Email, SessionKeys.Username);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        // GET: User/Register
+        public ActionResult Register()
+        {
+            return View("Register");
+        }
+
+        // POST: User/Register
+        [HttpPost]
+        public ActionResult Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Register", model);
+            }
+
+            Users user = _userDAL.GetUser(model.EmailAddress);
+            // Check to see if the username already exists
+            if (user != null)
+            {
+                ModelState.AddModelError("username-exists", "That email address is not available");
+                return View("Register", model);
+            }
+            else
+            {
+                // Convert from the ViewModel to a Data Model and Savae
+                user = new Users()
+                {
+                    Email = model.EmailAddress,
+                    Password = model.Password
+                };
+                _userDAL.SaveUser(user);
+
+                //FormsAuthentication.SetAuthCookie(user.Email, true);
+                //Session[SessionKeys.Username] = model.EmailAddress;
+                //Session[SessionKeys.UserId] = user.Id;
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        // POST: User/Logout
+        public ActionResult Logout()
+        {
+            //    FormsAuthentication.SignOut();
+            HttpContext.Session.Remove(SessionKeys.Username);
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- FinalCapstone/Dal/IUserDAL.cs
+++ FinalCapstone/Dal/IUserDAL.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FinalCapstone.Models;
 
 namespace FinalCapstone.Dal
 {
     public interface IUserDAL
     {
+        Users GetUser(string email);
+        void SaveUser(Users user);
     }
 }
