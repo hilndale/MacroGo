c5122fb8c57f104f4f762a1c2206aba379a08ee7
Keelie Bente <keelie.bente@gmail.com>
2018-12-13 13:56:45 -0500

    updating the macros works

--- FinalCapstone/Dal/IUserDAL.cs
+++ FinalCapstone/Dal/IUserDAL.cs
@@ -10,6 +10,7 @@ namespace FinalCapstone.Dal
     {
         Users GetUser(string email);
         void SaveUser(Users user);
+        bool UpdateGoals(UserProfileViewModel viewModel);
         UserProfileViewModel GetUserProfile(string email);
     }
 }
