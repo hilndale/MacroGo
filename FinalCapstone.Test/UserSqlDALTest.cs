0e8e23929dead11763b31c96fd09a8c5c28901ef
Hilary Sasso-Schleh <hsassoschleh@gmail.com>
2018-12-13 15:55:10 -0500

    created usersqldaltest class

---
+++ FinalCapstone.Test/UserSqlDALTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FinalCapstone.Dal;
+using System.Linq;
+using System.Data.SqlClient;
+using FinalCapstone.Models;
+using System.Transactions;
+
+namespace FinalCapstone.Test
+{
+    [TestClass]
+    public class UserSqlDALTest
+    {
+    }
+}
