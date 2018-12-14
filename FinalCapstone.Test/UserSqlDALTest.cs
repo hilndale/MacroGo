385a4ebc24d556a44db68809da6e06bf047880cb
Hilary Sasso-Schleh <hsassoschleh@gmail.com>
2018-12-14 08:58:48 -0500

    working on userdal tests

--- FinalCapstone.Test/UserSqlDALTest.cs
+++ FinalCapstone.Test/UserSqlDALTest.cs
@@ -51,6 +51,70 @@ namespace FinalCapstone.Test
             _userDAL.SaveUser(user);
 
             Assert.AreEqual("test@yahoo.com", user.Email);
-        }  
+        }
+
+
+        [TestMethod]
+        public void SaveAdminTest()
+        {
+            using (SqlConnection conn = new SqlConnection(MacroGoConnectionString))
+            {
+                conn.Open();
+
+                string sql = "INSERT INTO users ([Is_Admin], [Email], [Password], [Goal_Fat], [Goal_Protein], [Goal_Carbs]) VALUES (1, 'test@yahoo.com', 'password', 10, 15, 2000); SELECT CAST(SCOPE_IDENTITY() as int);";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                int userID = (int)cmd.ExecuteScalar();
+            }
+
+            Users user = new Users
+            {
+                IsAdmin = true,
+                Email = "test@yahoo.com",
+                Password = "password",
+                GoalFat = 10,
+                GoalProtein = 15,
+                GoalCarbs = 2000
+            };
+
+            _userDAL.SaveUser(user);
+
+            Assert.AreEqual(true, user.IsAdmin);
+        }
+
+
+        //public void UpdateGoalsTest()
+        //{
+        //    using(SqlConnection conn = new SqlConnection(MacroGoConnectionString))
+        //    {
+        //        conn.Open();
+
+        //        string sql = "INSERT INTO users ([Is_Admin], [Email], [Password], [Goal_Fat], [Goal_Protein], [Goal_Carbs]) VALUES (0, 'test@yahoo.com', 'password', 10, 15, 2000); SELECT CAST(SCOPE_IDENTITY() as int);";
+        //        SqlCommand cmd = new SqlCommand(sql, conn);
+        //        UserProfileViewModel user = (UserProfileViewModel)cmd.ExecuteScalar(); 
+        //    }
+
+        //    bool result = _userDAL.UpdateGoals(user);
+        //}
+
+        //public void GetUserTest()
+        //{
+        //    using(SqlConnection conn = new SqlConnection(MacroGoConnectionString))
+        //    {
+        //        conn.Open();
+
+        //        string sql = "INSERT INTO users ([Is_Admin], [Email], [Password], [Goal_Fat], [Goal_Protein], [Goal_Carbs]) VALUES (0, 'test@yahoo.com', 'password', 10, 15, 2000); SELECT CAST(SCOPE_IDENTITY() as int);";
+        //        SqlCommand cmd = new SqlCommand(sql, conn);
+
+        //       int id = (int)cmd.ExecuteScalar();
+
+        //    }
+
+        //    Users user =  _userDAL.GetUser(email);
+
+
+        //}
+
     }
+
+
 }
