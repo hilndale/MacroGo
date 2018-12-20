3de443cd374668bd8262de1d2595a36a8b47c056
Hilary Sasso-Schleh <hsassoschleh@gmail.com>
2018-12-20 12:21:11 -0500

    rest loc results now display on separate lines

--- FinalCapstone/wwwroot/js/TeamLimaLocationAPI.js
+++ FinalCapstone/wwwroot/js/TeamLimaLocationAPI.js
@@ -4,7 +4,7 @@
 // API model schema documentation: https://developers.zomato.com/documentation#!/restaurant/search //
 
 $(document).ready(function () {
-    
+
     $("#locationbutton").on("click", function (event) {
 
         if (navigator.geolocation) {
@@ -18,26 +18,25 @@ $(document).ready(function () {
             var longitude = position.coords.longitude;
 
             var restaurantChoice = $("#locationbutton").attr("name").toString();
-            if (restaurantChoice.includes(" "))
-            {
+            if (restaurantChoice.includes(" ")) {
 
                 var restaurantChoice = restaurantChoice.replace(" ", "");
 
             }
-   
+
             var url = "https://developers.zomato.com/api/v2.1/search?q=" + restaurantChoice + "&count=20&lat=" + latitude + "&lon=" + longitude + "&radius=16000&sort=real_distance&order=asc";
-                                                                                                                    
+
             $.ajax({
                 url: url,
                 type: "GET",
                 dataType: "json",
                 data: {
-                    "longitude" : longitude,
-                    "latitude" : latitude,
-                    "results_shown" : 5,
-                    "apikey" : "7df79b15f14cd152cc6a2a366d1be686",
-                    "results_start" : "0",
-                    "start" : "0",
+                    "longitude": longitude,
+                    "latitude": latitude,
+                    "results_shown": 5,
+                    "apikey": "7df79b15f14cd152cc6a2a366d1be686",
+                    "results_start": "0",
+                    "start": "0",
                     "count": 10,
                     "q": restaurantChoice
                 },
@@ -47,25 +46,24 @@ $(document).ready(function () {
 
                     var response = [];
 
-                        for (var i = 0; i < 5; i++)
-                        {
-                            var restaurantName = result.restaurants[i].restaurant.name;
-                            restaurantName = restaurantName.replace(" ", "");
-                            restaurantName = restaurantName.replace("'", "");
-                            var restaurantFilter = $("#locationbutton").attr("name");
-                            restaurantFilter = restaurantFilter.replace(" ", "");
-                            restaurantFilter = restaurantFilter.replace("'", "");
-                            if (restaurantName == restaurantFilter && restaurantName.length == restaurantFilter.length)
-                            {
-                                var restaurantAddress = result.restaurants[i].restaurant.location.address;                                
-                                response += "| " + restaurantAddress + " ";      
-                            }                           
+                    for (var i = 0; i < 5; i++) {
+                        var restaurantName = result.restaurants[i].restaurant.name;
+                        restaurantName = restaurantName.replace(" ", "");
+                        restaurantName = restaurantName.replace("'", "");
+                        var restaurantFilter = $("#locationbutton").attr("name");
+                        restaurantFilter = restaurantFilter.replace(" ", "");
+                        restaurantFilter = restaurantFilter.replace("'", "");
+                        if (restaurantName == restaurantFilter && restaurantName.length == restaurantFilter.length) {
+
+                            var restaurantAddress = result.restaurants[i].restaurant.location.address;
+                            response += restaurantAddress + "<br />";
                         }
-                        
-                        $("#conditions").html(response);
-                   
-                }                
+                    }
+
+                    $("#conditions").html(response);
+
+                }
             });
-        }       
-    });    
+        }
+    });
 });
